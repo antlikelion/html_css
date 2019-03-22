f091d4965bd196b42814fe08e7c697465743a5c0
Inuh, LEE <ignorant_woo@naver.com>
2019-03-22 21:28:18 +0900

    third

--- assignment1_3.py
+++ assignment1_3.py
@@ -1,6 +1,8 @@
 A = [20, 55, 67, 82, 45, 33, 90, 87, 100, 25]
 sum_A = 0
-for index in range(len(A)):
+index = 0
+while index < len(A):
     if A[index] >= 50:
             sum_A += A.pop(index)
+    index += 1
 print(sum_A)
